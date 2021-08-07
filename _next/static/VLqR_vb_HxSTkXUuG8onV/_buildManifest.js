d68a386eb906b6acf73271f13e7ea127d27b4832
Alice <alice.b.odette@outlook.com>
2021-08-07 17:22:04 +0800

    Deploy Next.js to gh-pages

---
+++ _next/static/VLqR_vb_HxSTkXUuG8onV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/224-8fc49d75d1c9fcd6a9f2.js","static/chunks/pages/index-224c1e0c5bac577e7954.js"],"/_error":["static/chunks/pages/_error-157366ffea25803f02fd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
